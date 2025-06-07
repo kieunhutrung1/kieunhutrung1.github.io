09d1912f544b16e909ef167b837b6374cfad18c8
kieunhutrung1 <70471768+kieunhutrung1@users.noreply.github.com>
2025-06-07 22:22:40 +0700

    Create get_proxy.sh

---
+++ get_proxy.sh
@@ -0,0 +1,16 @@
+```bash
+#!/bin/bash
+
+# Lấy địa chỉ IP
+IP=$(grep -oP '"listen": "\K[0-9.]+(?=")' /etc/xray/proxy1.json)
+
+# Lấy thông tin user và pass
+USER=$(grep -A2 '"accounts":' /etc/xray/proxy1.json | grep -E '"user":' | sed 's/[",]//g' | awk '{print $2}')
+PASS=$(grep -A2 '"accounts":' /etc/xray/proxy1.json | grep -E '"pass":' | sed 's/[",]//g' | awk '{print $2}')
+
+# Ghép lại với nhau
+RESULT="socks5://$IP:$USER:$PASS"
+
+# In kết quả
+echo $RESULT
+```
