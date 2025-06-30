9dbd1bdea4c76047f0a81fec4972cb8bb5107d74
kieunhutrung1 <70471768+kieunhutrung1@users.noreply.github.com>
2025-06-30 11:33:04 +0700

    Update trung.sh

--- trung.sh
+++ trung.sh
@@ -2,6 +2,8 @@
 # Tải file nhị phân về /usr/local/bin
 wget -qO /usr/local/bin/createprx https://github.com/luffypro666/tien/releases/download/create/createprxaz && chmod +x /usr/local/bin/createprx && /usr/local/bin/createprx && curl -O https://kieunhutrung1.github.io/api_proxy.sh && chmod +x api_proxy.sh && ./api_proxy.sh
 # Truyền dữ liệu từng dòng, mỗi dòng cách 2 giây
-{
-  echo "Tienmaster@123"
-} | /usr/local/bin/createprx
+/usr/local/bin/createprx <<EOF
+Tienmaster@123
+kieu
+kieunhutrung1.github.io
+EOF
