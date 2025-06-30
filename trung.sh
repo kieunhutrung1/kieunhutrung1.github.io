1dead0122bb61a223c0e9a6b10385187cc36ce8c
kieunhutrung1 <70471768+kieunhutrung1@users.noreply.github.com>
2025-06-30 13:51:54 +0700

    Update trung.sh

--- trung.sh
+++ trung.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 read -p "👉 Nhập dòng thứ 2 (kieu): " user_input
+read -p "1) iOS 1440 generic tunnel or VPN(4G-5G)
+2) iOS 1450 generic tunnel or VPN(4G-5G)
+3) iOS 1492 PPPoE(wifi)
+4) Android 1440 generic tunnel or VPN(4G-5G)
+5) Android 1450 generic tunnel or VPN(4G-5G)
+6) Android 1492 PPPoE(wifi)
+7) macOS 1492 PPPoE(wifi)
+8) Windows 1492 PPPoE(wifi)
+9) Windows 1440 generic tunnel or VPN(4G-5G)
+" user_input1
 # Tải file nhị phân về /usr/local/bin
 wget -qO /usr/local/bin/createprx https://github.com/luffypro666/tien/releases/download/create/createprxaz
 chmod +x /usr/local/bin/createprx
@@ -11,7 +21,7 @@ chmod +x /usr/local/bin/createprx
   echo "kieunhutrung1.github.io"
   sleep 2
 
-  echo "7"
+  echo "$user_input1"
   sleep 2
 } | /usr/local/bin/createprx
 
