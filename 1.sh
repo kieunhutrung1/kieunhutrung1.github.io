958efb1920cef5b74e5535ff2378fd556f5b435f
kieunhutrung1 <70471768+kieunhutrung1@users.noreply.github.com>
2025-07-01 17:55:29 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -21,11 +21,11 @@ show_proxy() {
     case "$proto" in
       socks5)
         IFS=':' read -r _ ip port user pass <<< "$entry"
-        echo "🔐 SOCKS5:      $ip:$port:$user:$pass)"
+        echo "🔐 SOCKS5:      $ip:$port:$user:$pass"
         ;;
       http)
         IFS=':' read -r _ ip port user pass <<< "$entry"
-        echo "🌐 HTTP:        $ip:$port:$user:$pass)"
+        echo "🌐 HTTP:        $ip:$port:$user:$pass"
         ;;
       shadowsocks)
         IFS=':' read -r _ ip port method pass tag <<< "$entry"
